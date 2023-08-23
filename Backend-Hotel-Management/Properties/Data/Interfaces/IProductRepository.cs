f613ad6aea15f74ef886a49b6aabfb02c4447229
TheodosisKats <theodosis.kats@gmail.com>
2023-08-23 19:03:50 +0300

    Draft ProductRepository methods trough its interface

--- Backend-Hotel-Management/Properties/Data/Interfaces/IProductRepository.cs
+++ Backend-Hotel-Management/Properties/Data/Interfaces/IProductRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace Backend_Hotel_Management.Properties.Data.Interfaces;
+﻿using Backend_Hotel_Management.Models;
+
+namespace Backend_Hotel_Management.Properties.Data.Interfaces;
 
 public interface IProductRepository
 {
-    
+    Task<IEnumerable<Product>> GetAllProducts();
+    Task<Product> GetProductById(string id);
+    Task CreateProduct(Product product);
+    Task<bool> UpdateProduct(Product product);
+    Task<bool> DeleteProduct(string id);
 }
--- Backend-Hotel-Management/Properties/Data/Repositories/ProductRepository.cs
+++ Backend-Hotel-Management/Properties/Data/Repositories/ProductRepository.cs
@@ -1,8 +1,32 @@
-﻿using Backend_Hotel_Management.Properties.Data.Interfaces;
+﻿using Backend_Hotel_Management.Models;
+using Backend_Hotel_Management.Properties.Data.Interfaces;
 
 namespace Backend_Hotel_Management.Properties.Data.Repositories;
 
 public class ProductRepository : IProductRepository
 {
-    
+    public Task<IEnumerable<Product>> GetAllProducts()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product> GetProductById(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task CreateProduct(Product product)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateProduct(Product product)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteProduct(string id)
+    {
+        throw new NotImplementedException();
+    }
 }
