ee02f2fa970b864031b82180623db74938051af9
TheodosisKats <theodosis.kats@gmail.com>
2023-08-23 18:54:06 +0300

    Update MongoDbContext and initialize connection Modify Program.cs : Added MongoDBContext singleton

--- Backend-Hotel-Management/Program.cs
+++ Backend-Hotel-Management/Program.cs
@@ -1,4 +1,5 @@
 using Backend_Hotel_Management.Extensions;
+using Backend_Hotel_Management.Properties.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddApplicationService(builder.Configuration);
+builder.Services.AddSingleton<MongoDbContext>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
--- Backend-Hotel-Management/Properties/Data/MongoDbContext.cs
+++ Backend-Hotel-Management/Properties/Data/MongoDbContext.cs
@@ -1,6 +1,20 @@
-﻿namespace Backend_Hotel_Management.Properties.Data;
+﻿using Backend_Hotel_Management.Models;
+using MongoDB.Driver;
+
+namespace Backend_Hotel_Management.Properties.Data;
 
 public class MongoDbContext
 {
-    
+    private readonly IMongoDatabase _db;
+
+    public MongoDbContext(IConfiguration configuration)
+    {
+        var mongoSettings = configuration.GetSection("MongoDB");
+        var connectionString = mongoSettings["ConnectionString"];
+        var client = new MongoClient(connectionString);
+        _db = client.GetDatabase(mongoSettings["DatabaseName"]);
+    }
+
+    public IMongoCollection<Product> Products =>
+        _db.GetCollection<Product>("Products");
 }
