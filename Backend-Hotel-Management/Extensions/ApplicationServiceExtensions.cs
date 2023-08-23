baf8e7f11bf12b3f302632613202ffd79c4f3bf8
TheodosisKats <theodosis.kats@gmail.com>
2023-08-23 18:42:02 +0300

    Create ApplicationServiceExtensions to clearly declare our services / app components, during the development

---
+++ Backend-Hotel-Management/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Backend_Hotel_Management.Extensions;
+
+public static class ApplicationServiceExtensions
+{
+    //Adds extra services to the program.cs allowing for a clearer declaration between app components
+    public static void AddApplicationService(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddCors();
+    }
+}
--- Backend-Hotel-Management/Program.cs
+++ Backend-Hotel-Management/Program.cs
@@ -1,8 +1,12 @@
+using Backend_Hotel_Management.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddApplicationService(builder.Configuration);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
